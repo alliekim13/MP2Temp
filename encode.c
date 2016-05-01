4e07de7d28c69733fe8b0b3d607f07f913ae7d57
Allie Kim <alliekim@Allies-MacBook-Pro.local>
2016-04-30 15:18:20 -1000

    Working code builder

--- encode.c
+++ encode.c
@@ -7,8 +7,8 @@
 #define FIELD_SIZE 8
 #define MAX_LINE 10000
 typedef struct codeEntry {
-	int ;
 	char code[256];
+	int codeLength;
 } codeEntry;
 
 void buildTree (Node ** nodeptrptr, char input[], int* i, int size)
@@ -16,18 +16,22 @@ void buildTree (Node ** nodeptrptr, char input[], int* i, int size)
 	if (*i >= size) return;
 	*nodeptrptr = (Node *)malloc(sizeof(Node));
 	if (input[*i] == '0') 
-	{								
+		{	printf("Child\n");							
 		(*i) += 1;
 		int j;
 		int dec = 0;
-		for (j=0;j<FIELD_SIZE-1;j++)
+		for (j=0;j<FIELD_SIZE;j++)
 		{
-			if (input[j]=='0'+1)
-				dec += pow((double)2, (double)(FIELD_SIZE-1-j));
+			printf("%c", input[j+(*i)]);
+			if (input[j+(*i)]=='1')
+				dec += pow((double)2, (double)(FIELD_SIZE-j-1));
 		}
 		(*nodeptrptr)->letter = dec;
 		(*nodeptrptr)->freq = 0.0;
+		(*nodeptrptr)->right = NULL;
+		(*nodeptrptr)->left = NULL;
 		(*i)+=8;
+		return;
 	}
 	(*nodeptrptr)->letter = -1;
 	(*nodeptrptr)->freq = 0;
@@ -37,13 +41,39 @@ void buildTree (Node ** nodeptrptr, char input[], int* i, int size)
 	buildTree(&((*nodeptrptr)->right), input, i, size);
 }
 
-codeEntry encodeEntry(Node * node, int key)
+void encodeEntry(Node * node, codeEntry entries[], char code[], int size)
 {
 	if (node == NULL) {
-		return -1;
+		return;
 	}
-	
+	//printf("Data: %i\n", node->letter);
+		printf("Hi\n");
+	if (node->left == NULL && node->right == NULL)
+	{
+		int i;
+		printf("Hi2\n");
+		for (i=0;i<size;i++) 
+			{	printf("Break1\n");
+			if (node->letter != -1)
+				entries[node->letter].code[i]=code[i]; 
+		}
+		return;
+	}
+	code[size] = '0';
+				printf("Break2\n");
+	encodeEntry(node->left, entries, code, size + 1);
+	code[size] = '1';
+	encodeEntry(node->right, entries, code, size + 1);
 }
+
+void VLRTraverseTree(Node * root) {
+if (root!= NULL) {
+	printf("Lett: %d\n", root->letter);
+	VLRTraverseTree(root->left);
+	VLRTraverseTree(root->right);
+}
+}
+
 int main(int argc, char *argv[])
 {
     FILE *fp1, *fp2, *fp3;
@@ -51,6 +81,7 @@ int main(int argc, char *argv[])
     char charcount[60];
     char freqline[50];
     char * p;
+	char code[256];
     Node * array[MAXSIZE];
     if (argc != 4) {
 		printf("Usage: createcode367 <codebook file> <data file> <compressed data file>\n");
@@ -74,11 +105,14 @@ int main(int argc, char *argv[])
 	fgets(fileLine, sizeof(fileLine), fp1);
 	printf("File size: %d\n", filesize);
 	buildTree(&root, fileLine, &i, filesize);
+	VLRTraverseTree(root);
 	codeEntry codebook[256];
-	for (j = 0; j<256; j++) 
-	{
-		codebook[j] = encodeEntry(root, j);
+	for (j = 0; j < 256; ++j) {
+		codebook[j].codeLength = 0;
 	}
+	encodeEntry(root, codebook, code, 0);
+	for (j=0;j<256;j++)
+		printf("%d = %s\n", j, codebook[j].code);
 	fclose(fp1);
 	fclose(fp2);
 	fclose(fp3);
